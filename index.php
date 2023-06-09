335ce2b41738466f115230d4f7b210156460fd16
fraolb <Fraolbereket@gmail.com>
2023-06-09 12:59:47 +0300

    python initialized

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+    Plugin Name: Living Lab Scale 1.0
+    Description: Give your rating !!
+    Version: 1.0
+    Author: Fraol
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; //exit if accessed directly
+
+class Scale {
+    function __construct() {
+        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+        add_action('enqueue_block_assets', array($this, 'frontendAssets')); // Enqueue CSS for the frontend
+    }
+
+    function adminAssets() {
+        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+    }
+
+    function frontendAssets() {
+        wp_enqueue_style('scale-frontend-styles', plugin_dir_url(__FILE__) . 'build/index.css');
+    }
+}
+
+$scale = new Scale();
